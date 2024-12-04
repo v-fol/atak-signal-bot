281d377359f1de6bd85e00aa5d3ad42c2c0ecb47
Volodymyr Foliush <flenderinfo@gmail.com>
2024-12-04 17:56:25 +0200

    [feature] created client for websocket and rest comunication with Signal rest api

--- signal-listener-publisher/app/client.py
+++ signal-listener-publisher/app/client.py
@@ -1,27 +1,33 @@
 import aiohttp
-import asyncio
 import orjson
 
 from typing import AsyncGenerator
 
 class SignalClient:
-    def __init__(self, signal_cli_rest_api_domain: str):
-        self.signal_cli_rest_api_domain = signal_cli_rest_api_domain
+    """
+    SignalClient class to interact with Signal REST API
+    """
+    def __init__(self, signal_cli_rest_api_url: str):
+        self.signal_cli_rest_api_url = signal_cli_rest_api_url
 
     async def listen(self, bot_phone_number: str) -> AsyncGenerator[dict, None]:
+        """
+        Listen for messages from the Signal REST API WebSocket
+        """
         async with aiohttp.ClientSession() as session:
             async with session.ws_connect(
-                f"http://{self.signal_cli_rest_api_domain}:8080/v1/receive/{bot_phone_number}"
+                f"http://{self.signal_cli_rest_api_url}/v1/receive/{bot_phone_number}"
             ) as ws:
                 async for msg in ws:
                     yield orjson.loads(msg.data)
 
-    #         curl -X POST -H "Content-Type: application/json" 'http://localhost:8080/v2/send' \
-    #  -d '{"message": "Test", "number": "+380931124245", "recipients": [ "+380967455345" ]}'
     async def send_text_message(self, bot_phone_number: str, recipients: list[str], message: str) -> dict:
+        """
+        Send a text message to a list of Signal recipients
+        """
         async with aiohttp.ClientSession() as session:
             async with session.post(
-                f"http://{self.signal_cli_rest_api_domain}:8080/v2/send",
+                f"http://{self.signal_cli_rest_api_url}/v2/send",
                 json={"message": message, "number": bot_phone_number, "recipients": recipients},
             ) as resp:
                 if resp.status == 404:
