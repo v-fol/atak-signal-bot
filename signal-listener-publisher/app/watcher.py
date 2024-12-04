04e48645694193025e577f3465ff387e370ba54c
Volodymyr Foliush <flenderinfo@gmail.com>
2024-12-05 00:50:56 +0200

    [bugfix] minimal fixes

--- signal-listener-publisher/app/watcher.py
+++ signal-listener-publisher/app/watcher.py
@@ -1,30 +1,31 @@
 import os
+import time
 import signal
 import subprocess
-import time
 import logging
 
-from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
 
 from config import SCRIPT_TO_RUN
 
 logging.basicConfig(level=logging.INFO)
 
+
 DIRECTORY_TO_WATCH = os.path.dirname(os.path.abspath(SCRIPT_TO_RUN))
 
-
 class ScriptReloader(FileSystemEventHandler):
-    def __init__(self):
+    def __init__(self, script_to_run: str):
         self.process = None
+        self.script_to_run = script_to_run
         self.start_script()
 
     def start_script(self):
-        logging.info(f"Starting script: {SCRIPT_TO_RUN}")
-        self.process = subprocess.Popen(["python", SCRIPT_TO_RUN])
+        logging.info(f"Starting script: {self.script_to_run}")
+        self.process = subprocess.Popen(["python", self.script_to_run])
 
     def restart_script(self):
-        logging.info(f"Restarting script: {SCRIPT_TO_RUN}")
+        logging.info(f"Restarting script: {self.script_to_run}")
         self.process.send_signal(signal.SIGINT)
         self.process.wait()
         self.start_script()
@@ -36,7 +37,7 @@ class ScriptReloader(FileSystemEventHandler):
 
 if __name__ == "__main__":
     logging.info(f"Watching directory: {DIRECTORY_TO_WATCH}")
-    event_handler = ScriptReloader()
+    event_handler = ScriptReloader(SCRIPT_TO_RUN)
     observer = Observer()
     observer.schedule(event_handler, DIRECTORY_TO_WATCH, recursive=True)
     logging.info(f"Watching directory: {DIRECTORY_TO_WATCH}")
