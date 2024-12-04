fee736ad9fe738fa1203eee0d096e3c6bcc36fad
Volodymyr Foliush <flenderinfo@gmail.com>
2024-12-04 17:57:28 +0200

    [feature] decoupled configs and refactored app entry point

--- signal-listener-publisher/app/main.py
+++ signal-listener-publisher/app/main.py
@@ -1,75 +1,99 @@
 import asyncio
-import json
+import orjson
 import os
 import logging
 
-from confluent_kafka import Producer
-
 from client import SignalClient
 from message_processor import SignalMessage, HandleAction
+from publisher import KafkaPublisher as Producer
+from config import TOPIC, KAFKA_BROKER, SIGNAL_CLI_REST_API_URL, BOT_PHONE_NUMBER
 
-# Configuration
-TOPIC = "signal"
-SIGNAL_CLI_REST_API_DOMAIN = os.getenv("SIGNAL_CLI_REST_API_DOMAIN", "localhost")
-KAFKA_BROKER = os.getenv("KAFKA_BROKER", "kafka:9092")
-
-signal_client = SignalClient(SIGNAL_CLI_REST_API_DOMAIN)
-
-# Logging setup
 logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
+logging.info("Starting signal-listener-publisher")
+logging.info(f"Running in mode: {os.getenv('MODE', 'none')}")
 
-logger.info("Starting signal-listener-publisher")
-logger.info(f"Connecting to {os.getenv('MODE', 'none')}")
 
-# Kafka producer setup
-kafka_config = {'bootstrap.servers': KAFKA_BROKER}
-producer = Producer(kafka_config)
+class SignalListenerPublisher:
+    """Handles listening to Signal messages and publishing to Kafka."""
 
-def delivery_report(err, msg):
-    """Callback for Kafka delivery reports."""
-    if err:
-        logger.error(f"Message delivery failed: {err}")
-    else:
-        logger.info(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+    def __init__(self, signal_client: SignalClient, producer: Producer):
+        self.signal_client = signal_client
+        self.producer = producer
 
-async def connect():
-    """Connect to SignalClient and listen for messages."""
-    async for message in signal_client.listen("+380931124245"):
-        logger.debug(f"Received message: {message}")
-        # await signal_client.send(message["account"], [message["envelope"]["source"], ],  "Message received")
+    async def process_signal_message(self, message: dict) -> None:
+        """
+        Processes an incoming Signal message.
+
+        Routes, processes, and decides whether to respond via Signal or broker.
+        """
         signal_message = SignalMessage.from_dict(message).route_message()
 
         if signal_message.handle_action == HandleAction.IGNORE:
-            continue
+            # logging.debug("Message ignored.")
+            return
 
         if signal_message.handle_action == HandleAction.SEND_MESSAGE:
-            await signal_client.send_text_message(
+            logging.info("Sending response back to Signal.")
+            await self.signal_client.send_text_message(
                 signal_message.account,
                 [signal_message.envelope["sourceNumber"]],
-                signal_message.response_message
+                signal_message.response_message,
             )
-            continue
+            return
 
-        try:
-            producer.produce(
-                TOPIC,
-                value=signal_message.brocker_message.encode("utf-8"),
-                callback=delivery_report
+        await self.publish_to_broker(signal_message)
+
+    async def publish_to_broker(self, signal_message: SignalMessage) -> None:
+        """
+        Publishes a message to Kafka
+        """
+        if self.producer.error_cb_triggered:
+            self.producer = Producer(KAFKA_BROKER, TOPIC)
+
+        self.producer.publish(orjson.dumps(signal_message.brocker_message))
+        
+        if self.producer.error_cb_triggered:
+            logging.error("Broker error during publishing. Sending error message to Signal.")
+            await self.signal_client.send_text_message(
+                signal_message.account,
+                [signal_message.envelope["sourceNumber"]],
+                "Server error. Please try again later.",
             )
-            # Trigger Kafka delivery callbacks
-            producer.poll(1)
-            producer.flush()
-            logger.info("Message sent to Kafka")
-        except Exception as e:
-            logger.error(f"Failed to process message: {e}")
+    
+    async def listen(self) -> None:
+        """
+        Listens for incoming messages from Signal.
+        """
+        async for message in self.signal_client.listen(BOT_PHONE_NUMBER):
+            await self.process_signal_message(message)
 
-if __name__ == "__main__":
+    def close(self) -> None:
+        """
+        Closes the Kafka producer and any other resources.
+        """
+        if not self.producer.error_cb_triggered:
+            self.producer.close()
+
+
+def main():
+    """
+    Main entry point for the application.
+    """
+    signal_client = SignalClient(SIGNAL_CLI_REST_API_URL)
+    producer = Producer(KAFKA_BROKER, TOPIC)
+    app = SignalListenerPublisher(signal_client, producer)
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
+
     try:
-        loop.run_until_complete(connect())
+        loop.run_until_complete(app.listen())
     except KeyboardInterrupt:
-        logger.info("Shutting down...")
+        logging.info("Shutting down gracefully...")
     finally:
-        producer.flush()
+        app.close()
+        loop.close()
+
+
+if __name__ == "__main__":
+    main()
